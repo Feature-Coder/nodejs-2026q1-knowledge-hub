21ce24954976e19a50fff0b3abe928d6bc9a073b
Aleksandr Volkov <featurecoder.dev@gmail.com>
2026-04-03 19:45:08 +0300

    feat: add in-memory data store for entities
    Adds public arrays for `User`, `Article`, `Category`, and `Comment` entities
    to the `DatabaseService`, establishing an in-memory data store.
    
    Exports `DatabaseService` from `DatabaseModule` to make these collections
    accessible for injection across the application.

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { Article } from 'src/article/entities/article.entity';
+import { Category } from 'src/category/entities/category.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService {
+  public users: User[] = [];
+  public articles: Article[] = [];
+  public categories: Category[] = [];
+  public comments: Comment[] = [];
+}

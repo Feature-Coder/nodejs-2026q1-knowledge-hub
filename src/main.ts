930aa634880053a3720c837db6e450634304301a
Aleksandr Volkov <featurecoder.dev@gmail.com>
2026-04-03 13:24:54 +0300

    feat: add swagger api documentation
    Integrates SwaggerModule to serve API documentation from `doc/api.yaml`.
    Allows port configuration via `process.env.PORT`.
    
    Adds `@nestjs/swagger` and `yaml` dependencies.
    Updates `.gitignore` to exclude environment variable files.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import * as yaml from 'yaml';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const file = readFileSync('./doc/api.yaml', 'utf8');
+  const document = yaml.parse(file);
+  SwaggerModule.setup('doc', app, document);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/doc`);
 }
 bootstrap();
